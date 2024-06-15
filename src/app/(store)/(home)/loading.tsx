da738e81c92d2d2bc484578a106bfc733cb76dda
Joao Paulo <joaopauloj405@gmail.com>
2024-06-15 19:32:15 -0300

    feat: add skeleton loading to home

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,12 @@
+import Skeleton from '@/components/Skeleton'
 import React from 'react'
 
 export default function Loading() {
-  return <div>loading</div>
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 has-[856px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
+    </div>
+  )
 }
--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,7 +4,9 @@ import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    cache: 'no-store',
+  })
   const products = await response.json()
   return products
 }
